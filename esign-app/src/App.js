49d1d9cc3c5d827904f5f196ac6925e055b74c4f
rukmani4746 <you@example.com>
2023-06-01 16:16:02 +0530

    reusable component title is ready

---
+++ esign-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Title from './components/Title';
+
+const App = () => {
+return (
+  <div>
+    <h1 className="text-center">Signature</h1>
+    <Title text="name"/>
+    <Title text="Date"/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, inventore! Fugit quae nostrum, excepturi accusamus pariatur quibusdam odio neque! Placeat quibusdam quos veniam velit ex voluptate fugit asperiores aspernatur facere culpa? Quos eius, fugit illum ipsam impedit facilis ea at ducimus sapiente asperiores, possimus aperiam velit dignissimos? Reiciendis harum aliquam esse quo, corrupti delectus corporis unde sapiente laboriosam distinctio omnis praesentium consectetur repellendus. Suscipit libero sunt atque amet reiciendis! Consectetur perferendis quas, quos soluta deleniti architecto a placeat odit accusamus repellat reiciendis amet suscipit cum? Explicabo quam quibusdam, eligendi neque veniam labore magnam facere ipsa maiores, maxime laborum quidem voluptates. </p>
+
+  </div>
+  
+)
+}
+
+export default App;
+
+
